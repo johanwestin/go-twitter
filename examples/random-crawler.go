04031fb406b7bd60148bdc8b3f5a860aaffab3f5
Jacob Winn <jmwinn21@gmail.com>
2012-04-03 22:15:29 -0400

    Ran 'go fix' on all subdirectories

--- examples/random-crawler.go
+++ examples/random-crawler.go
@@ -3,51 +3,51 @@ package main
 import "twitter"
 import "fmt"
 import "time"
-import "rand"
+import "math/rand"
 
 const kMaxDepth = 25
-const kStart = "jb55"
+const kStart = "jacob_winn"
 
 var api *twitter.Api
 var r *rand.Rand
 var done chan bool
 
 func main() {
-  api = twitter.NewApi()
-  done = make(chan bool)
-  r = rand.New(rand.NewSource(time.Seconds()))
-  crawl(kStart, 0)
-  <-done
+	api = twitter.NewApi()
+	done = make(chan bool)
+	r = rand.New(rand.NewSource(time.Now()))
+	crawl(kStart, 0)
+	<-done
 }
 
 func crawl(userName string, level int) {
-  // Get the user's status
-  text := (<-api.GetUser(userName)).GetStatus().GetText()
+	// Get the user's status
+	text := (<-api.GetUser(userName)).GetStatus().GetText()
 
-  for i := 0; i < level; i++ {
-    fmt.Printf("  ")
-  }
+	for i := 0; i < level; i++ {
+		fmt.Printf("  ")
+	}
 
-  fmt.Printf("%s: %s\n", userName, text)
+	fmt.Printf("%s: %s\n", userName, text)
 
-  level++
-  if level > kMaxDepth {
-    done <- true
-    return
-  }
+	level++
+	if level > kMaxDepth {
+		done <- true
+		return
+	}
 
-  // Get the user's friends
-  friends := <-api.GetFriends(userName, 1)
-  length := len(friends)
+	// Get the user's friends
+	friends := <-api.GetFriends(userName, 1)
+	length := len(friends)
 
-  if length == 0 {
-    done <- true
-    return
-  }
+	if length == 0 {
+		done <- true
+		return
+	}
 
-  rVal := r.Intn(length - 1)
-  // Choose a random friend for the next user
-  nextUser := friends[rVal].GetScreenName()
+	rVal := r.Intn(length - 1)
+	// Choose a random friend for the next user
+	nextUser := friends[rVal].GetScreenName()
 
-  go crawl(nextUser, level)
+	go crawl(nextUser, level)
 }
